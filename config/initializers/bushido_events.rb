fc04ac266700b051cc4c61272d13969169882f8e
Akash Manohar J <akash@akash.im>
2011-11-25 03:44:52 +0530

    removed logger statement
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- config/initializers/bushido_events.rb
+++ config/initializers/bushido_events.rb
@@ -4,8 +4,6 @@ module Bushido
   def self.subscribe_to_events
     ::Bushido::Data.listen("mail.received") do |event|
       letter = Letter.new
-      logger.debug "{event.inspect}"
-
       letter.payload = event[:data].to_json
       letter.save
     end
