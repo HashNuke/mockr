4d91101761328d654f363571d5c594e1e136b85f
Akash Manohar J <akash@akash.im>
2011-11-25 01:04:15 +0530

    added bushido gem to environment.rb; added events subscribe code to bushido initializer
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- config/initializers/bushido_events.rb
+++ config/initializers/bushido_events.rb
@@ -1,1 +1,16 @@
+require 'bushido'
+
+module Bushido
+  def self.subscribe_to_events
+    ::Bushido::Data.listen("mail.received") do |event|
+      letter = Letter.new
+      logger.debug "{event.inspect}"
+
+      letter.payload = event[:data].to_json
+      letter.save
+    end
+  end
+end
+
+
 Bushido.subscribe_to_events
