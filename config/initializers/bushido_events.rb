2c1d5ff24fccf1e63d31c434e92487c0411213d6
Akash Manohar J <akash@akash.im>
2011-11-25 23:10:09 +0530

    inspecting event param
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- config/initializers/bushido_events.rb
+++ config/initializers/bushido_events.rb
@@ -4,10 +4,10 @@ module Bushido
   def self.subscribe_to_events
     ::Bushido::Data.listen("mail.received") do |event|
       STDOUT.puts "=====MAIL RECEIVED===="
-      STDOUT.puts "#{event[:data].inspect}"
+      STDOUT.puts "#{event.inspect}"
+      STDOUT.puts "=====END-OF-MAIL======"
     end
   end
 end
-
 
 Bushido.subscribe_to_events
